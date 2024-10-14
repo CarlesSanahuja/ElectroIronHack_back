2a975aa5a383abd9b29c36299a58d95f856e6a96
CSanahuja <csanahuja@sapalomera.cat>
2024-10-14 13:47:58 +0200

    14/10 2

--- src/main/java/com/example/demo/EletrodomesticosIronHackBack.java
+++ src/main/java/com/example/demo/EletrodomesticosIronHackBack.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class EletrodomesticosIronHackBack {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EletrodomesticosIronHackBack.class, args);
 	}
 
 }
